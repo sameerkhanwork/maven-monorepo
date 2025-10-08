7226a4a3d5b9dd13cf980355b39c51235f4e078a
Mohd Sameer Khan <sameerkhanwork1@gmail.com>
2025-10-08 18:31:19 +0530

    changed env to secrets

--- Common/src/main/java/com/ai/connector/common/Utils.java
+++ Common/src/main/java/com/ai/connector/common/Utils.java
@@ -2,6 +2,6 @@ package com.ai.connector.common;
 
 public class Utils {
     public static String greet(String name) {
-        return "Hello, " + name + " from Common!!";
+        return "Hello, " + name + " from Common!!!";
     }
 }
