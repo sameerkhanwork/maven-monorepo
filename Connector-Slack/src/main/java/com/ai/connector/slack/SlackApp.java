f110ccb9100af0407721e75fdef8c6c974a28790
Mohd Sameer Khan <sameerkhanwork1@gmail.com>
2025-10-09 12:01:15 +0530

    success, now testing slack and teams

--- Connector-Slack/src/main/java/com/ai/connector/slack/SlackApp.java
+++ Connector-Slack/src/main/java/com/ai/connector/slack/SlackApp.java
@@ -4,6 +4,6 @@ import com.ai.connector.common.Utils;
 
 public class SlackApp {
     public static void main(String[] args) {
-        System.out.println(Utils.greet("Slack testing"));
+        System.out.println(Utils.greet("Slack working good"));
     }
 }
