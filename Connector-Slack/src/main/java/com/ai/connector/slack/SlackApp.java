5b5a9211e966399813df13975e6878a48cc8bf0e
Mohd Sameer Khan <sameerkhanwork1@gmail.com>
2025-10-09 12:58:32 +0530

    success, now testing slack only

--- Connector-Slack/src/main/java/com/ai/connector/slack/SlackApp.java
+++ Connector-Slack/src/main/java/com/ai/connector/slack/SlackApp.java
@@ -4,6 +4,6 @@ import com.ai.connector.common.Utils;
 
 public class SlackApp {
     public static void main(String[] args) {
-        System.out.println(Utils.greet("Slack working good (Slack Only)!!!❤️🤞"));
+        System.out.println(Utils.greet("Slack working good (Slack Only)!!!❤️🤞❤️"));
     }
 }
